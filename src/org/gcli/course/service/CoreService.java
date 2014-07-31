a3184609ae95c50320079d60dc5e15b7fc977ce3
gcli <liguochun0313@qq.com>
2014-07-31 20:03:01 +0800

    ？修正

--- src/org/gcli/course/service/CoreService.java
+++ src/org/gcli/course/service/CoreService.java
@@ -37,7 +37,6 @@ public class CoreService {
 			Map<String, String> requestMap = MessageUtil.parseXml(request);
 			// 发送方帐号（open_id）
 			String fromUserName = requestMap.get("FromUserName");
-			log.debug("fromUserName:" + fromUserName);
 			// 公众帐号
 			String toUserName = requestMap.get("ToUserName");
 			// 消息类型
@@ -55,6 +54,7 @@ public class CoreService {
 			contentMsg.append("您好，我是机器人，请回复数字选择服务：").append("\n\n");
 			contentMsg.append("1  彩票开奖信息").append("\n");
 			contentMsg.append("2  天气信息").append("\n");
+			contentMsg.append("3  聊天唠嗑").append("\n");
 
 			textMessage.setContent(contentMsg.toString());
 			// 将文本消息对象转换成xml字符串
@@ -64,7 +64,9 @@ public class CoreService {
 				// 接收用户发送的文本消息内容
 				String content = requestMap.get("Content");
 
-				if ("1".equals(content) || "彩票".equals(content)) {
+				if ("?".equals(content)||"？".equals(content)) {
+
+				} else if ("1".equals(content) || "彩票".equals(content)) {
 					contentMsg = new StringBuffer();
 					GetLotteryByURL cp = new GetLotteryByURL();
 					Lottery[] fc = cp.getLotterys();
@@ -77,19 +79,62 @@ public class CoreService {
 									+ fc[1].getOpencode() + "。开奖时间："
 									+ fc[1].getOpentime() + "。");
 
-					textMessage.setContent(contentMsg.toString());
-
 				} else if ("2".equals(content) || "天气".equals(content)) {
 					contentMsg = WeatherSevice.getWeather("大连");
-					textMessage.setContent(contentMsg.toString());
 
-				} else {
-					String str = Chat4XiaoI.Tess(content, "crazylee");
-					textMessage.setContent(str);
+				} else if ("3".equals(content)) {
+					contentMsg = new StringBuffer();
+					contentMsg
+					.append("聊天唠嗑使用说明")
+					.append("\n").append("闲来无聊，找我唠嗑吧。我很能聊的，有问必答！例如：")
+					.append("\n").append("讲个笑话")
+					.append("\n").append("大连有什么好玩的")
+					.append("\n").append("特价机票");
+
+				}else {
+					contentMsg = new StringBuffer();
+					contentMsg.append(Chat4XiaoI.Tess(content, "crazylee"));
 
 				}
 
 			}
+			// 图片消息
+			else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_IMAGE)) {
+
+			}
+			// 地理位置消息
+			else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_LOCATION)) {
+
+			}
+			// 链接消息
+			else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_LINK)) {
+
+			}
+			// 音频消息
+			else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_VOICE)) {
+
+			}
+			// 事件推送
+			else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_EVENT)) {
+				// 事件类型
+				String eventType = requestMap.get("Event");
+				// 订阅
+				if (eventType.equals(MessageUtil.EVENT_TYPE_SUBSCRIBE)) {
+					contentMsg = new StringBuffer();
+					contentMsg
+							.append("亲，你咋才来呢，我想你了。我能为您提供天气，彩票，公交，聊天，笑话等服务，是您生活娱乐好帮手！")
+							.append("\n").append("回复\"？\"显示主菜单");
+				}
+				// 取消订阅
+				else if (eventType.equals(MessageUtil.EVENT_TYPE_UNSUBSCRIBE)) {
+					// TODO 取消订阅后用户再收不到公众号发送的消息，因此不需要回复消息
+				}
+				// 自定义菜单点击事件
+				else if (eventType.equals(MessageUtil.EVENT_TYPE_CLICK)) {
+					// TODO 自定义菜单权没有开放，暂不处理该类消息
+				}
+			}
+			textMessage.setContent(contentMsg.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
